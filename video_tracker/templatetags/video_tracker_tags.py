a6e82a9d6066520fbec0e8abb228ca712b19b5b3
Jason Ward <jason.ward@policystat.com>
2011-02-22 14:28:12 -0500

    had the templates deal with which user has seen which video

--- video_tracker/templatetags/video_tracker_tags.py
+++ video_tracker/templatetags/video_tracker_tags.py
@@ -4,7 +4,22 @@ from video_tracker.models import Video
 
 register = template.Library()
 
+#TODO Always dont show to guest user
 @register.simple_tag
-def get_video_url(name):
+def get_video_url(name, user):
     video = Video.objects.get(name=name)
     return video.url
+
+@register.simple_tag
+def get_video(name, user):
+    video = Video.objects.get(name=name)
+    if user in video.users_viewed.all():
+        return ''
+    video.add_user(user)
+    return '''
+	<script type="text/javascript">
+		$(document).ready(function() {
+			show_video("%s");
+		});
+	</script>''' % video.url
+
