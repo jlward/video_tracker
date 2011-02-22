d115b7af06cdd56e050adaecee86b28ab65cb5cb
Jason Ward <jason.ward@policystat.com>
2011-02-22 11:23:34 -0500

    added initial migration

---
+++ video_tracker/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Video'
+        db.create_table('video_tracker_video', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50, db_index=True)),
+        ))
+        db.send_create_signal('video_tracker', ['Video'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Video'
+        db.delete_table('video_tracker_video')
+
+
+    models = {
+        'video_tracker.video': {
+            'Meta': {'object_name': 'Video'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['video_tracker']
